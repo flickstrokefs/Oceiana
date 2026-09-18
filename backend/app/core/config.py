7d9971d0a25a9fdcd0b24bad256733a1b98f4a03
Agnaypv <agnaypv3@gmail.com>
2026-09-18 12:01:42 +0530

    Add ARIEL landing page

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -22,6 +22,7 @@ class Settings(BaseSettings):
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "http://oceianaxoxo.vercel.app",
         "*",
     ]
 
